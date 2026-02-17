d3eea1bccba77fa3d6ca8953ad0854ebdde2ec5e
A-sambhav <sambhava06@gmail.com>
2026-02-17 22:57:31 +0530

    Format code for consistency in weapon_relay.ino

--- weapon_relay.ino
+++ weapon_relay.ino
@@ -1,15 +1,15 @@
 // Define pins
-const int inputPin = 2;   // Signal input pin
-const int relayPin = 8;   // Relay control pin
+const int inputPin = 2;  
+const int relayPin = 8;   
 
 void setup() {
-  pinMode(inputPin, INPUT);      // Input pin
-  pinMode(relayPin, OUTPUT);     // Relay pin
-  digitalWrite(relayPin, LOW);   // Ensure relay starts OFF
+  pinMode(inputPin, INPUT);      
+  pinMode(relayPin, OUTPUT);     
+  digitalWrite(relayPin, LOW);   
 }
 
 void loop() {
-  int inputState = digitalRead(inputPin); // Read input signal
+  int inputState = digitalRead(inputPin); 
 
   if (inputState == HIGH) {
     digitalWrite(relayPin, HIGH); // Turn relay ON
